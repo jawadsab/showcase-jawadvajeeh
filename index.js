657ec12413f24c626869ddb10889cf005389ec9e
Mohammed Abdul Jawad Vajeeh <jawadsab.v@gmail.com>
2024-11-11 06:16:36 +0300

    added profession command to the cli

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
 console.log('Mohammed Abdul Jawad Vajeeh - Software Engineer!🖥️');
+
+const [, , command, ...args] = process.argv;
+
+if (command) {
+	switch (command) {
+		case "profession":
+			displayProfession();
+			break;
+
+		default:
+			console.log("Unknown command. Available commands: profession");
+	}
+}
+
+
+function displayProfession() {
+	console.log('Jawad Vajeeh is a software engineer')
+}
